d6979411714e556f9820cdc8c27ea635b89c4fdc
Caidz <kay.ess.emm@gmail.com>
2021-01-21 12:27:42 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang = "en-US">
+<head>
+	<meta charset = "UTF-8" />
+	<title>ETF Calculator</title>
+</head>
+<body>
+<?php
+	print "<h1>Hello world!</h1>";
+?>
+</body>
+</html>
